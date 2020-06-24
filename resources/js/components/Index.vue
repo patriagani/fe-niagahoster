7c6cb7f51ef5bf0175e594815bfe028b931ee754
patria gani <patria.gani@gmail.com>
2020-06-24 09:41:17 +0700

    initial commit

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Halooo</h1>
+</template>
+
+
+<script>
+    export default {
+        data() {
+            return {
+                posts: []
+            }
+        },
+        created() {
+            
+        }
+    }
+</script>
